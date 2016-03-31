ceba3580434e09ee100beddf139fd5602b50c00c
Adam Miskiewicz <adam@sk3vy.com>
2016-03-31 13:19:19 -0400

    Make sure we reference the .default export of transform-legacy-decorators.

--- decorator-support.js
+++ decorator-support.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [require('babel-preset-react-native')],
   plugins: [
-    require('babel-plugin-transform-decorators-legacy'),
+    require('babel-plugin-transform-decorators-legacy').default,
     require('babel-plugin-transform-do-expressions'),
     require('babel-plugin-transform-function-bind'),
     require('babel-plugin-transform-class-constructor-call'),
